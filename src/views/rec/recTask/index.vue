8d32685b7ed56af20f726ed8e3ae7a3565fb711b
allen <987171620@qq.com>
2025-08-11 17:27:28 +0800

    优化： 任务页面修改

--- src/views/rec/recTask/index.vue
+++ src/views/rec/recTask/index.vue
@@ -5,11 +5,11 @@
         <el-card shadow="hover">
           <el-form ref="queryFormRef" :model="queryParams" :inline="true">
             <el-form-item label="标题" prop="title">
-              <el-input v-model="queryParams.title" placeholder="请输入标题" clearable @keyup.enter="handleQuery" />
+              <el-input style="width: 200px" v-model="queryParams.title" placeholder="请输入标题" clearable @keyup.enter="handleQuery" />
             </el-form-item>
             <el-form-item label="状态" prop="status">
               <el-select v-model="queryParams.status" placeholder="请选择状态" clearable>
-                <el-option v-for="dict in task_status" :key="dict.value" :label="dict.label" :value="dict.value" />
+                <el-option style="width: 200px" v-for="dict in task_status" :key="dict.value" :label="dict.label" :value="dict.value" />
               </el-select>
             </el-form-item>
             <el-form-item label="截止日期" style="width: 308px">
@@ -55,15 +55,15 @@
       <el-table v-loading="loading" :data="recTaskList" @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55" align="center" />
         <el-table-column label="主键ID" align="center" prop="id" v-if="false" />
-        <el-table-column label="标题" align="center" prop="title" />
-        <el-table-column label="描述" align="center" prop="description" />
-        <el-table-column label="状态" align="center" prop="status">
+        <el-table-column label="标题" align="center" prop="title" min-width="120" />
+        <el-table-column label="描述" align="center" prop="description" min-width="240" />
+        <el-table-column label="状态" align="center" prop="status" min-width="80" fixed="right">
           <template #default="scope">
             <dict-tag :options="task_status" :value="scope.row.status" />
           </template>
         </el-table-column>
-        <el-table-column label="进度" align="center" prop="progress" />
-        <el-table-column label="截止日期" align="center" prop="deadLine" width="180">
+        <el-table-column label="进度" align="center" prop="progress" min-width="80" />
+        <el-table-column label="截止日期" align="center" prop="deadLine" min-width="120">
           <template #default="scope">
             <span>{{ parseTime(scope.row.deadLine, '{y}-{m}-{d}') }}</span>
           </template>
@@ -73,10 +73,17 @@
             <dict-tag :options="priority" :value="scope.row.priority" />
           </template>
         </el-table-column>
-        <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <el-table-column label="操作" align="center" class-name="small-padding fixed-width" min-width="120" fixed="right">
           <template #default="scope">
             <el-tooltip content="修改" placement="top">
               <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['rec:recTask:edit']"></el-button>
+            </el-tooltip>
+            <el-tooltip content="详情" placement="top">
+              <el-button link type="primary" @click="handleDetail(scope.row)" v-hasPermi="['msg:msgUser:detail']">
+                <template #default>
+                  <img src="@/assets/mes/Frame2.png" />
+                </template>
+              </el-button>
             </el-tooltip>
             <el-tooltip content="删除" placement="top">
               <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['rec:recTask:remove']"></el-button>
@@ -88,34 +95,50 @@
       <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
     </el-card>
     <!-- 添加或修改任务对话框 -->
-    <el-dialog :title="dialog.title" v-model="dialog.visible" width="500px" append-to-body>
-      <el-form ref="recTaskFormRef" :model="form" :rules="rules" label-width="80px">
+    <el-dialog @opened="handleDialogOpened" :title="dialog.title" v-model="dialog.visible" width="500px" append-to-body>
+      <el-form class="card-container" ref="recTaskFormRef" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="标题" prop="title">
-          <el-input v-model="form.title" placeholder="请输入标题" />
+          <el-input ref="titleInputRef" v-model="form.title" placeholder="请输入标题" :disabled="isDetailView" />
         </el-form-item>
         <el-form-item label="描述" prop="description">
-          <el-input v-model="form.description" type="textarea" placeholder="请输入内容" />
+          <el-input v-model="form.description" type="textarea" placeholder="请输入内容" :disabled="isDetailView" />
         </el-form-item>
         <el-form-item label="状态" prop="status">
-          <el-select v-model="form.status" placeholder="请选择状态">
+          <el-select v-model="form.status" placeholder="请选择状态" :disabled="isDetailView">
             <el-option v-for="dict in task_status" :key="dict.value" :label="dict.label" :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="进度" prop="progress">
-          <el-input v-model="form.progress" placeholder="请输入进度" />
+          <el-input-number
+            :disabled="isDetailView"
+            style="width: 100%"
+            v-model="form.progress"
+            :min="0"
+            :max="100"
+            :step="1"
+            placeholder="请输入进度"
+          />
         </el-form-item>
         <el-form-item label="截止日期" prop="deadLine">
-          <el-date-picker clearable v-model="form.deadLine" type="date" value-format="YYYY-MM-DD" placeholder="请选择截止日期"></el-date-picker>
+          <el-date-picker
+            :disabled="isDetailView"
+            clearable
+            v-model="form.deadLine"
+            type="date"
+            value-format="YYYY-MM-DD"
+            placeholder="请选择截止日期"
+            style="width: 100%"
+          ></el-date-picker>
         </el-form-item>
-        <el-form-item label="优先级" prop="priority">
-          <el-select v-model="form.priority" placeholder="请选择优先级">
+        <el-form-item label="优先级" prop="priority" style="margin-bottom: 2px; width: 100%">
+          <el-select v-model="form.priority" placeholder="请选择优先级" :disabled="isDetailView">
             <el-option v-for="dict in priority" :key="dict.value" :label="dict.label" :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
       </el-form>
       <template #footer>
         <div class="dialog-footer">
-          <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+          <el-button :loading="buttonLoading" type="primary" @click="submitForm" v-if="!isDetailView">确 定</el-button>
           <el-button @click="cancel">取 消</el-button>
         </div>
       </template>
@@ -126,6 +149,8 @@
 <script setup name="RecTask" lang="ts">
 import { listRecTask, getRecTask, delRecTask, addRecTask, updateRecTask } from '@/api/rec/recTask';
 import { RecTaskVO, RecTaskQuery, RecTaskForm } from '@/api/rec/recTask/types';
+import { ref, reactive, toRefs, getCurrentInstance, onMounted, nextTick } from 'vue';
+import { ElInput } from 'element-plus';
 
 const { proxy } = getCurrentInstance() as ComponentInternalInstance;
 const { task_status } = toRefs<any>(proxy?.useDict('task_status'));
@@ -143,6 +168,7 @@ const dateRangeDeadLine = ref<[DateModelType, DateModelType]>(['', '']);
 
 const queryFormRef = ref<ElFormInstance>();
 const recTaskFormRef = ref<ElFormInstance>();
+const isDetailView = ref(false); // 是否为详情查看模式
 
 const dialog = reactive<DialogOption>({
   visible: false,
@@ -171,6 +197,7 @@ const data = reactive<PageData<RecTaskForm, RecTaskQuery>>({
   rules: {
     title: [{ required: true, message: '标题不能为空', trigger: 'blur' }],
     description: [{ required: true, message: '描述不能为空', trigger: 'blur' }],
+    priority: [{ required: true, message: '优先级不能为空', trigger: 'change' }],
     status: [{ required: true, message: '状态不能为空', trigger: 'change' }]
   }
 });
@@ -186,6 +213,20 @@ const getList = async () => {
   recTaskList.value = res.rows;
   total.value = res.total;
   loading.value = false;
+};
+
+const titleInputRef = ref<InstanceType<typeof ElInput> | null>(null);
+const handleDialogOpened = () => {
+  console.log('handleDialogOpened');
+  nextTick(() => {
+    if (titleInputRef.value) {
+      // 获取输入框的DOM元素并聚焦
+      const inputEl = titleInputRef.value.input;
+      if (inputEl) {
+        inputEl.focus();
+      }
+    }
+  });
 };
 
 /** 取消按钮 */
@@ -223,6 +264,9 @@ const handleSelectionChange = (selection: RecTaskVO[]) => {
 /** 新增按钮操作 */
 const handleAdd = () => {
   reset();
+  isDetailView.value = false;
+  form.value.priority = 'high';
+  form.value.status = 'pending';
   dialog.visible = true;
   dialog.title = '添加任务';
 };
@@ -230,11 +274,23 @@ const handleAdd = () => {
 /** 修改按钮操作 */
 const handleUpdate = async (row?: RecTaskVO) => {
   reset();
+  isDetailView.value = false;
   const _id = row?.id || ids.value[0];
   const res = await getRecTask(_id);
   Object.assign(form.value, res.data);
   dialog.visible = true;
   dialog.title = '修改任务';
+};
+
+/** 查询详情 */
+const handleDetail = async (row?: RecTaskVO) => {
+  reset();
+  isDetailView.value = true;
+  const _id = row?.id || ids.value[0];
+  const res = await getRecTask(_id);
+  Object.assign(form.value, res.data);
+  dialog.visible = true;
+  dialog.title = '查看任务';
 };
 
 /** 提交按钮 */
@@ -257,7 +313,7 @@ const submitForm = () => {
 /** 删除按钮操作 */
 const handleDelete = async (row?: RecTaskVO) => {
   const _ids = row?.id || ids.value;
-  await proxy?.$modal.confirm('是否确认删除任务编号为"' + _ids + '"的数据项？').finally(() => (loading.value = false));
+  await proxy?.$modal.confirm('是否确认删除任务标题为"' + (row?.title || '选中') + '"的数据项？').finally(() => (loading.value = false));
   await delRecTask(_ids);
   proxy?.$modal.msgSuccess('删除成功');
   await getList();
@@ -278,3 +334,6 @@ onMounted(() => {
   getList();
 });
 </script>
+
+/* 使用common.scss中全部样式 */
+<style lang="scss" src="@/assets/styles/vue-column.scss" />
